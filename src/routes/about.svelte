ddfe8df31fba0b03c71620f83307b5a86dbbf394
Me Me <me@me.com>
2022-04-27 23:16:54 +0000

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<script>
+
+
+
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, excepturi? Dolorum quam tempore rerum, a tenetur dolore modi voluptatem consequuntur unde excepturi corrupti perferendis sapiente natus, ipsam totam ut distinctio!
+    </p>
+    <a href="/guides">guides</a>
+    <a href="/">home</a>
+</div>
+
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+
+
+</script>
+
+<div class="index">
+    <h2>Some title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum sunt, sint ab incidunt obcaecati tempora animi optio doloribus exercitationem unde dignissimos possimus neque ducimus. Veritatis eius doloribus optio cupiditate dolores?
+    </p>
+    <a href="/guides">guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+.index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+}
+</style>
